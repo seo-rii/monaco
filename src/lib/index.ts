15152fa05474bb9cc3b25a076b5bce762e2a947a
seorii <me@seorii.page>
2026-02-21 03:19:06 +0000

    fix: add types

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,4 +1,11 @@
 import Monaco from './Monaco.svelte';
-export type { IMonacoSetting } from './Monaco.svelte';
+export type {
+	IMonacoInputEvent,
+	IMonacoSetting,
+	IMonacoSnippet,
+	IMonacoSnippetLoader,
+	IMonacoSnippetMap,
+	IMonacoSnippetRegister
+} from './Monaco.svelte';
 
 export default Monaco;
